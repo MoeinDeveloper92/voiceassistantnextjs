9b632f8dfa0e8716a25e4fda409466ff737454ad
moein samani <moeinsamani1992@gmail.com>
2025-01-09 11:01:42 +0100

    implment strutcture

--- core/store/sharedStore.ts
+++ core/store/sharedStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { SuggestionItem } from '../types/type';
-
+import { Message } from '../types/type';
 interface SharedState {
   suggestions: SuggestionItem[];
   response: string;
